9190ec92a23ace986b3342c3af5e8de20c6e9329
Daniel Paleyev <danny.paleyev@gmail.com>
2023-12-04 07:43:44 +0300

    Multi scale discriminator

--- hw_nv/model/discriminator.py
+++ hw_nv/model/discriminator.py
@@ -62,3 +62,69 @@ class MultiPeriodDiscriminator(nn.Module):
         return outs, features
 
 
+class ScaleDiscriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv_blocks = nn.ModuleList([
+            nn.Sequentional(
+                nn.utils.weight_norm(nn.Conv1d(1, 16, kernel_size=15, stride=1, padding=7)),
+                nn.LeakyReLU(),
+            ),
+            nn.Sequentional(
+                nn.utils.weight_norm(nn.Conv1d(16, 64, kernel_size=41, stride=4, groups=4, padding=20)),
+                nn.LeakyReLU(),
+            ),
+            nn.Sequentional(
+                nn.utils.weight_norm(nn.Conv1d(64, 256, kernel_size=41, stride=4, groups=16, padding=20)),
+                nn.LeakyReLU(),
+            ),
+            nn.Sequentional(
+                nn.utils.weight_norm(nn.Conv1d(256, 1024, kernel_size=41, stride=4, groups=64, padding=20)),
+                nn.LeakyReLU(),
+            ),
+            nn.Sequentional(
+                nn.utils.weight_norm(nn.Conv1d(1024, 1024, kernel_size=41, stride=4, groups=256, padding=20)),
+                nn.LeakyReLU(),
+            ),
+            nn.Sequentional(
+                nn.utils.weight_norm(nn.Conv1d(1024, 1024, kernel_size=5, stride=1, padding=2)),
+                nn.LeakyReLU(),
+            ),
+            nn.utils.weight_norm(nn.Conv1d(1024, 1, kernel_size=3, stride=1, padding=1)),
+        ])
+    
+    def forward(self, x):
+        features = []
+        for conv_block in self.conv_blocks:
+            x = conv_block(x)
+            features.append(x)
+        return x.reshape(x.shape[0], -1), features
+    
+
+class MultiScaleDiscriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.descriminators = nn.ModuleList([
+            ScaleDiscriminator()
+            for i in range(3)
+        ])
+
+        self.pooling = nn.ModuleList([
+            nn.AvgPool1d(kernel_size=4, stride=2, padding=1),
+            nn.AvgPool1d(kernel_size=4, stride=2, padding=1)
+        ])
+    
+    def forward(self, x):
+        outs = []
+        features = []
+        for i in range(3):
+            x_in = x.copy()
+            for j in range(i):
+                x_in = self.pooling[j](x_in)
+            out, feature = self.descriminators[i](x_in)
+            outs.append(out)
+            features.append(feature)
+        return outs, features
+            
+
