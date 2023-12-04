01559a918221a8040b2a78e38ebcb41d57c91b7c
Daniel Paleyev <danny.paleyev@gmail.com>
2023-12-04 14:14:16 +0300

    Loss

---
+++ hw_nv/loss/HiFiLoss.py
@@ -0,0 +1,52 @@
+import torch
+from torch import nn
+from hw_nv.utils import MelSpectrogram, MelSpectrogramConfig
+
+class HiFiLoss(nn.Module):
+    def __init__(self, fm_lambda, mel_lambda) -> None:
+        super().__init__()
+        self.fm_lambda = fm_lambda
+        self.mel_lambda = mel_lambda
+        self.mel_spectrogram = MelSpectrogram(MelSpectrogramConfig)
+    
+    def forward(self, mel, gen,
+                period_gen,
+                period_gen_features,
+                period_real,
+                period_real_features,
+                scale_gen,
+                scale_gen_features,
+                scale_real,
+                scale_real_features, **kwargs):
+        
+        gen_spectrogram = self.mel_spectrogram(gen.squeeze(1))
+        spectrogram = nn.ConstantPad3d(padding=(0, 0, 0, 0, 0, gen_spectrogram.shape[-1] - spectrogram.shape[-1]), value=0)(mel)
+
+        adv_loss = 0
+        for periods in period_gen:
+            adv_loss = adv_loss + torch.mean((periods - 1) ** 2)
+
+        for scales in scale_gen:
+            adv_loss = adv_loss + torch.mean((scales - 1) ** 2)
+        
+
+        fm_loss = 0
+        for real, gen in zip(period_real_features, period_gen_features):
+            fm_loss = fm_loss + torch.nn.functional.l1_loss(gen, real)
+
+        for real, gen in zip(scale_real_features, scale_gen_features):
+            fm_loss = fm_loss + torch.nn.functional.l1_loss(gen, real)
+        
+        mel_loss = torch.nn.functional.l1_loss(gen_spectrogram, spectrogram)
+
+        gen_loss = adv_loss + self.fm_lambda * fm_loss + self.mel_lambda * mel_loss
+
+        desc_loss = 0
+        for real, gen in zip(period_real, period_gen):
+            desc_loss = desc_loss + torch.mean((real - 1) ** 2) + torch.mean(gen ** 2)
+        for real, gen in zip(scale_real, scale_gen):
+            desc_loss = desc_loss + torch.mean((real - 1) ** 2) + torch.mean(gen ** 2)
+        
+        return desc_loss, gen_loss, adv_loss, fm_loss, mel_loss
+
+        
